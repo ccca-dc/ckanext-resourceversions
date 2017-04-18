986e901381ce45ff4660bf633073860331fe70fa
KatharinaSack <katharina.sack@ccca.ac.at>
2017-04-18 10:36:50 +0200

    updating of resource file will create new resource

--- ckanext/resourceversions/plugin.py
+++ ckanext/resourceversions/plugin.py
@@ -4,6 +4,7 @@ import ckan.plugins.toolkit as toolkit
 
 class ResourceversionsPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IResourceController, inherit=True)
 
     # IConfigurer
 
@@ -11,3 +12,24 @@ class ResourceversionsPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'resourceversions')
+
+    new_res_version = ""
+
+    # IResourceController
+    def before_update(self, context, current, resource):
+        # toolkit.check_access('package_update', context, resource)
+        if resource['upload'] != "" or "/" in resource['url'] and current['url'] != resource['url']:
+            # create new resource with the new file/link
+            global new_res_version
+            new_res_version = resource.copy()
+            new_res_version.pop('id', None)
+            new_res_version['package_id'] = current['package_id']
+            new_res_version['name'] = "new version"
+
+            # change the resource that will be updated to the old version
+            resource.clear()
+            resource.update(current.copy())
+
+    def after_update(self, context, resource):
+        # add new version to package
+        toolkit.get_action('resource_create')(context, new_res_version)
