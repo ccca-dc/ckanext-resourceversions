19c2a853f850dd02aee93e4b033e90cf93549317
KatharinaSack <katharina.sack@ccca.ac.at>
2017-04-20 11:08:02 +0200

    list of versions now on resource page

--- ckanext/resourceversions/helpers.py
+++ ckanext/resourceversions/helpers.py
@@ -12,10 +12,19 @@ global_contains_field = []
 
 def get_versions_list(resource_id, package_id):
     ctx = {'model': model}
-    versions = []
+
     pkg = logic.get_action('package_show')(ctx, {'id': package_id})
+    resource = logic.get_action('resource_show')(ctx, {'id': resource_id})
     resource_list = pkg['resources']
-    for resource in resource_list:
-        if 'newerVersion' in resource and resource['newerVersion'] == resource_id:
-            return resource['id']
-    return ""
+
+    res_helper = resource
+    versions = []
+    while res_helper is not None:
+        res_helper_id = res_helper['id']
+        res_helper = None
+        for res in resource_list:
+            if 'newerVersion' in res and res['newerVersion'] == res_helper_id:
+                versions.append({'id': res['id'], 'name': res['name']})
+                res_helper = res.copy()
+                break
+    return versions
